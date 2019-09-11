bacfb60ca962997a0b524bf5a9ea775627bb405a
Jansen Sampurna <jansensampurna98@gmail.com>
2019-09-11 08:57:22 +0700

    Change Folder Name to Week05

---
+++ Week05/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+    { path: '', redirectTo: 'places', pathMatch: 'full' },
+    { path: 'bookings', loadChildren: './bookings/bookings.module#BookingsPageModule'},
+    { path: 'places', loadChildren: './places/places.module#PlacesPageModule' },
+    { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },  { path: 'create-booking', loadChildren: './bookings/create-booking/create-booking.module#CreateBookingPageModule' },
+
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    ],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
